283fcf3bd9dc35c69c5859dba56de66eb0fe2d85
vivek <vivekvardhan166@gmail.com>
2023-10-12 00:57:44 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+let person = {
+    name: 'Vivek',
+    age: 25
+};
+ person.name = 'Vicky';
+console.log(person);
+
+let selectedColors = ['red', 'purple'];
+
+function first(message) {
+    console.log('first message is ' + message);
+}
+first('my own message');
+// Factory Function
+function createCircle(radius){
+    return {
+        radius,
+        draw: function() {
+            console.log('drawing with radius '+ radius);
+        }
+    };
+}
+let circle = createCircle(4);
+// console.log(circle);
+circle.draw();
+//Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('drawing with radius ' + radius);
+    }
+}
+let another = new Circle(3);
+another.draw();
+//window.alert('Hello broooo');
